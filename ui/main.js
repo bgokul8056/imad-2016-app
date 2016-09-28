96fdc3a07c025ff42de15ad1b62b0ef78d8d2100
Gokula krishnan <gokulakrishnan1996@gmail.com>
2016-09-28 05:30:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,17 @@
 console.log('Loaded!');
-var x=document.getElementById('content').innerHTML="Changed the COntent";
+
+
+var x=document.getElementById('content');
+x.innerHTML="Changed the COntent";
+
+
+var img=document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+};
+img.onclick=function(){
+    var variable= setInterval(moveRight,50);
+};
